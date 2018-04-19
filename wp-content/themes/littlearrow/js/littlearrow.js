f163ffee449062c64bccd06a87cbf3e9292cf578
Azmol-Miah <azmol@azmolmiah.com>
2018-04-18 22:52:07 -0700

    trying to workout how to add the maps section may think about using ACF for the whole site or future wordpress sites

--- wp-content/themes/littlearrow/js/littlearrow.js
+++ wp-content/themes/littlearrow/js/littlearrow.js
@@ -166,7 +166,7 @@ for (i = 0; i < classTime.length; i++) {
         
         //Icon Size
         var icon = {
-            url: "img/logo.png", // url
+            url: "../img/logo.png", // url
             scaledSize: new google.maps.Size(50, 50), // scaled size
             origin: new google.maps.Point(0,0), // origin
             anchor: new google.maps.Point(30, 45) // anchor
